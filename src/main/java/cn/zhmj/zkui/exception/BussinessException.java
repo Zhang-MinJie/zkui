c5434e91f05ada8afc929588334bb8ef80a3f054
zhangminjie <2366373158@qq.com>
2018-10-31 16:13:25 +0800

    eeeeeeeee

--- src/main/java/cn/zhmj/zkui/exception/BussinessException.java
+++
@@ -1,9 +0,0 @@
-package cn.zhmj.zkui.exception;
-
-public class BussinessException extends RuntimeException {
-
-	private static final long serialVersionUID = 1L;
-	public BussinessException(Object message) {
-		super(String.valueOf(message));
-	}
-}
